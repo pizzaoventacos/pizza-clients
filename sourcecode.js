a569bdc3a91ac02934af1d2feca792ccaac8bdad
pizzaoventacos <82193692+pizzaoventacos@users.noreply.github.com>
2021-05-19 21:16:53 -0700

    Update sourcecode.js

--- sourcecode.js
+++ sourcecode.js
@@ -8,10 +8,10 @@
       this.description = client && typeof client == "object" &&  client.description ? client.description : 'No description was provided'
       this.keywords = client && typeof client == "object" &&  client.keywords && Array.isArray(client.keywords) ? `- ${client.keywords.join(", - ")}` : "No keywords were provided."
       this.otherInfo = client && typeof client == "object" &&  client.otherInfo ? client.otherInfo : null
-    }
-    getInfo() {
+    } //creates an object with customizable options that are read through an object and defaulting to certain values
+   getInfo() {
       return `Hello! My name is: ${this.name}.\nI am used for/a ${this.purpose}!\nCurrently, I am on version ${this.version}.\nI will give you a description of me and my abilities: ${this.description}\nSome Words that categorize/generalize my usage are: ${this.keywords}.\nHere is some other info about me: ${this.otherInfo}`
-    }
+    } //creates a method that returns a simple paragraph containing key information
     missingProp() {
       var missingprop = [];
       if(this.name == "Default Name") missingprop.push('name');
@@ -21,6 +21,6 @@
       if(this.keywords == "No keywords were provided.") missingprop.push('keywords');
       if(this.otherInfo == null) missingprop.push('otherInfo')
       return missingprop;
-    }
+    } //creates a method using an empty array. Then it checks each property and if the value is the default value, it adds the name of the property to the array, then it returns the array,
   }
  }
