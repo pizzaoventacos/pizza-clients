47b9de199cde6b93498457032355f7abd73aa05b
pizzaoventacos <82193692+pizzaoventacos@users.noreply.github.com>
2021-05-19 19:47:09 -0700

    Update Example.js

--- Example.js
+++ Example.js
@@ -9,4 +9,39 @@ const client = new Clients.Client
     version: 1.0,
     otherInfo: 'This client-sorter is one of my first decent-sized projects.'
   }
-)
+);
+console.log(client); /*
+returns: 
+Client {
+  name: 'A testing client',
+  purpose: 'This client is used for: Git Example',
+  version: 1,
+  description: 'A testing client that can be used to sort information that I would like to keep track of.',
+  keywords: '- Example, - Organized',
+  otherInfo: 'This client-sorter is one of my first decent-sized projects.'
+*/
+console.log(client.getInfo()); /*
+returns: 
+Hello! My name is: A testing client.
+I am used for/a This client is used for: Git Example!
+Currently, I am on version 1.
+I will give you a description of me and my abilities: A testing client that can be used to sort information that I would like to keep track of.
+Some Words that categorize/generalize my usage are: - Example, - Organized.
+Here is some other info about me: This client-sorter is one of my first decent-sized projects.
+*/
+console.log(client.missingProp()); /*
+returns: 
+[]
+if we did not define everything, it would return every property that is the default value.
+if we defined client as this:
+const client = new Clients.Client()
+it would return: 
+[
+  'name',
+  'purpose',
+  'version',
+  'description',
+  'keywords',
+  'otherInfo'
+]
+*/
